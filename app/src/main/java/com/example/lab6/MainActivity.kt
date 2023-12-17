ea88da8ffb37ea41fcd0da547d0179fbeb9c8cbc
miki799 <46634713+miki799@users.noreply.github.com>
2023-12-17 20:55:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.lab6
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lab6.ui.theme.LAB6Theme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LAB6Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App({
+                        sendPostRequest()
+                    })
+
+                }
+            }
+        }
+    }
+
+    private var trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+        object : X509TrustManager {
+            override fun checkClientTrusted(
+                certs: Array<X509Certificate?>?, authType: String?
+            ) {
+            }
+            override fun checkServerTrusted(
+                certs: Array<X509Certificate?>?, authType: String?
+            ) {
+            }
+            override fun getAcceptedIssuers():
+                    Array<X509Certificate> {
+                return emptyArray()
+            }
+        }
+    )
+
+    private fun sendPostRequest() {
+        GlobalScope.launch {
+//            val sc = SSLContext.getInstance("SSL")
+//            sc.init(null, trustAllCerts, SecureRandom())
+//            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+
+            val url = URL("https://jsonplaceholder.typicode.com/posts")
+            val con: HttpURLConnection = url.openConnection() as HttpURLConnection
+            con.requestMethod = "GET"
+
+            val inputStream = BufferedReader(
+                InputStreamReader(con.inputStream)
+            )
+            var inputLine: String?
+            val content = StringBuffer()
+            while (inputStream.readLine().also { inputLine = it } != null) {
+                content.append(inputLine)
+            }
+            inputStream.close()
+
+            con.disconnect()
+
+            Log.d("send_get_request", content.toString())
+        }
+    }
+
+
+}
+
+@Composable
+fun App(sendPostRequest: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Click the button and check logcat!",
+            modifier = modifier
+        )
+        Button(
+            modifier = modifier,
+            onClick = {
+                sendPostRequest()
+            }
+        ) {
+            Text(stringResource(R.string.post_request))
+        }
+    }
+}
